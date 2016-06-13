6194109502f249c2b58d30789ddfd88d7c48fb99
Giles Hall <giles@ginkgobioworks.com>
2016-06-13 14:04:46 -0400

    lots of fixes and enhancements

--- ncbi-blast-db.py
+++ ncbi-blast-db.py
@@ -18,10 +18,13 @@ class ThreadQueue(object):
     def __init__(self):
         self.que = []
         self.que_cv = threading.Condition()
+        self._join = False
 
     def deque(self):
         self.que_cv.acquire()
         if len(self.que) == 0:
+            if self._join:
+                return None
             self.que_cv.wait()
         item = self.que.pop()
         self.que_cv.release()
@@ -33,6 +36,18 @@ class ThreadQueue(object):
         self.que_cv.notify()
         self.que_cv.release()
 
+    def join(self):
+        self.que_cv.acquire()
+        self._join = True
+        self.que_cv.notify_all()
+        self.que_cv.release()
+    
+    def flush(self):
+        self.que_cv.acquire()
+        self.que = []
+        # XXX: notify?
+        self.que_cv.release()
+
     def __iter__(self):
         while 1:
             item = self.deque()
@@ -41,20 +56,20 @@ class ThreadQueue(object):
             yield item
 
 class DownloadThread(threading.Thread):
-    def __init__(self, connection):
-        self.connection = connection
-        self.work_que = ThreadQueue()
-        self.finished_que = ThreadQueue()
+    def __init__(self, connectf, inq, outq):
+        self.connection = connectf()
+        self.inq = inq
+        self.outq = outq
         super(DownloadThread, self).__init__()
         self.daemon = True
 
     def run(self):
         try:
-            for work in self.work_que:
+            for work in self.inq:
                 if self.download(*work):
-                    self.finished_que.enque(work)
+                    self.outq.enque(work)
         finally:
-            self.finished_que.enque(None)
+            self.outq.enque(None)
 
     def download(self, target, filename_hash):
         md5hash = md5.new()
@@ -82,7 +97,8 @@ class NCBI_BlastMirror(object):
     NCBI_BLAST_DIR = "/blast/db"
     HASH_FILENAME = "blastdb.md5"
 
-    def __init__(self, blastdb_dir=None, archive_dir=None, config_path=None, hashfile=None, include=None, exclude=None):
+    def __init__(self, blastdb_dir=None, archive_dir=None, config_path=None, hashfile=None, include=None, exclude=None, threads=None):
+        self.threads = threads if threads != None else 1
         self.blastdb_dir = blastdb_dir if blastdb_dir != None else os.getcwd()
         self.archive_dir = archive_dir if archive_dir != None else os.path.join(self.blastdb_dir, "archives")
         self.config_path = config_path if config_path != None else os.path.join(self.blastdb_dir, ".config.ini")
@@ -114,7 +130,7 @@ class NCBI_BlastMirror(object):
             return hashmap
         with open(self.hashfile, 'r') as fh:
             for line in fh:
-                (md5hash, filename) = self.re_whitespace.split(line.strip())
+                (filename, md5hash) = self.re_whitespace.split(line.strip())
                 hashmap[filename] = md5hash
         return hashmap
 
@@ -133,6 +149,9 @@ class NCBI_BlastMirror(object):
         hashes = [fn for fn in files if fn.endswith(".md5")]
         dblist = set([fn.split('.')[0] for fn in hashes])
         dblist = set(self.filter_dblist(dblist))
+        if len(dblist) == 0:
+            msg = "inclusion / exclusion rules filtered out all possible matches, manifest empty"
+            logging.warning(msg)
         hashmap = {}
         for fn in hashes:
             dbname = fn.split('.')[0]
@@ -147,19 +166,31 @@ class NCBI_BlastMirror(object):
         self.remote_hashmap = self.load_remote_hashmap()
         for fn in self.remote_hashmap:
             if fn in self.local_hashmap and self.local_hashmap[fn] == self.remote_hashmap[fn]:
+                target = os.path.join(self.archive_dir, fn)
+                if os.path.exists(target):
+                    os.unlink(target)
                 continue
             yield fn
 
+    def build_download_workers(self):
+        self.download_que = ThreadQueue()
+        self.finished_que = ThreadQueue()
+        workers = []
+        for idx in range(self.threads):
+            worker = DownloadThread(self.connect, self.download_que, self.finished_que)
+            worker.start()
+            workers.append(worker)
+        return workers
+
     def sync(self):
-        self.connect()
-        download_thread = DownloadThread(self.ncbi)
-        download_thread.start()
+        self.ncbi = self.connect()
+        self.workers = self.build_download_workers()
         for fn in self.manifest():
             target = os.path.join(self.archive_dir, fn)
             work = (target, self.remote_hashmap[fn])
-            download_thread.work_que.enque(work)
-        download_thread.work_que.enque(None)
-        for (target, local_hash) in download_thread.finished_que:
+            self.download_que.enque(work)
+        self.download_que.join()
+        for (target, local_hash) in self.finished_que:
             filename = os.path.split(target)[-1]
             if not self.install(target):
                 msg = "%s failed to uncompress" % target
@@ -167,6 +198,7 @@ class NCBI_BlastMirror(object):
                 continue
             self.local_hashmap[fn] = self.remote_hashmap[fn]
             self.write_local_hashmap(self.local_hashmap)
+            os.unlink(target)
 
     def filter_dblist(self, dblist):
         def match(dbname, pattern_list):
@@ -176,10 +208,11 @@ class NCBI_BlastMirror(object):
         return filter(filter_rule, dblist)
 
     def connect(self):
-        self.ncbi = ftplib.FTP(self.NCBI_HOST)
-        self.ncbi.set_debuglevel(0)
-        self.ncbi.login()
-        self.ncbi.cwd(self.NCBI_BLAST_DIR)
+        ncbi = ftplib.FTP(self.NCBI_HOST)
+        ncbi.set_debuglevel(0)
+        ncbi.login()
+        ncbi.cwd(self.NCBI_BLAST_DIR)
+        return ncbi
 
     def download_hash(self, fn):
         content = []
@@ -216,6 +249,7 @@ def get_cli():
     parser.add_argument("-a", "--archive_dir", help="path to archive directory")
     parser.add_argument("-H", "--hashfile", help="path to local hash file")
     parser.add_argument("-i", "--init", action="store_true", help="initialize BLAST database")
+    parser.add_argument("-t", "--threads", help="Number of download threads")
     defaults = {
         "blastdb_dir": os.environ.get("BLASTDB", None),
         "archive_dir": None,
